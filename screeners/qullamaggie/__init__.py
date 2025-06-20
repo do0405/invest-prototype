136f50263626301c7bdb5c8241614949405f32c1
do0405 <108686184+do0405@users.noreply.github.com>
2025-06-20 14:42:05 +0900

    Fix __init__ exports

--- screeners/qullamaggie/__init__.py
+++ screeners/qullamaggie/__init__.py
@@ -2,11 +2,15 @@
 # 쿨라매기 매매법 알고리즘 모듈
 
 # 주요 모듈 임포트
-from .screener import (
+from .core import (
+    apply_basic_filters,
     screen_breakout_setup,
+    check_vcp_pattern,
     screen_episode_pivot_setup,
     screen_parabolic_short_setup,
 )
+
+from .screener import run_qullamaggie_screening
 
 from .signal_generator import (
     generate_buy_signals,
@@ -17,12 +21,15 @@ from .signal_generator import (
 from .runner import run_qullamaggie_strategy
 
 __all__ = [
+    "apply_basic_filters",
     "screen_breakout_setup",
+    "check_vcp_pattern",
     "screen_episode_pivot_setup",
     "screen_parabolic_short_setup",
     "generate_buy_signals",
     "generate_sell_signals",
     "manage_positions",
+    "run_qullamaggie_screening",
     "run_qullamaggie_strategy",
 ]
 
